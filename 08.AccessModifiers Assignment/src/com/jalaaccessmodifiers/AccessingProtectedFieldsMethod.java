5d19ce8853e09d07f05c80db6aa9e3f2c7b70138
teju0509 <103272647+teju0509@users.noreply.github.com>
2022-04-08 22:08:43 +0530

    Access Modifiers

---
+++ 08.AccessModifiers Assignment/src/com/jalaaccessmodifiers/AccessingProtectedFieldsMethod.java
@@ -0,0 +1,28 @@
+/**
+ * @author JALA Academy
+ *
+ * 3. Also, Access the PROTECTED fields and methods from child class located
+ * in a different package
+ * <p>
+ * - Access the PROTECTED fields and methods from any class in different package
+ */
+package com.jala.pack2;
+
+//importing different package
+import com.jala.accessmodifiers.ProtectedClassFieldsMethod;
+
+//Accessing the PROTECTED fields and methods from child class
+public class AccessingProtectedFieldsMethod extends ProtectedClassFieldsMethod {
+
+    public static void main(String[] args) {
+        //Creating object of AccessingProtectedFieldsMethod
+        AccessingProtectedFieldsMethod sub = new AccessingProtectedFieldsMethod();
+        //Accessing protected field
+        sub.name = "Protected Method in SubClass";
+        //Accessing protected method
+        sub.protectedMethod();
+    }
+    /* NOTE :
+             - protected member can be access within the package and
+                outside the package but within the child classes */
+}
