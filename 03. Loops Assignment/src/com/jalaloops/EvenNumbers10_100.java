a987a75af75e8bea54f22552df6fd52df7be6814
teju0509 <103272647+teju0509@users.noreply.github.com>
2022-04-08 22:04:10 +0530

    Loops

---
+++ 03. Loops Assignment/src/com/jalaloops/EvenNumbers10_100.java
@@ -0,0 +1,20 @@
+/**
+ * @author JALA Academy
+ * Website Reference : https://java.jalaacademy.com/java/java_loops.html
+ *
+ * 6. Write a program to print even number between 10 and 100 using while
+ */
+package com.jala.loops;
+
+public class EvenNumbers10_100 {
+    public static void main(String[] args) {
+
+        System.out.println("The Even Numbers between 10 and 100 are : ");
+        int i = 10;
+        //loop executes from 10 to 100
+        while (i <= 100) {
+            System.out.println(i);
+            i += 2;
+        }
+    }
+}
---
+++ 03. Loops Assignment/src/com/jalaloops/LargestAmongNumbers.java
@@ -0,0 +1,37 @@
+/**
+ * @author JALA Academy
+ * Website Reference : https://java.jalaacademy.com/java/java_loops.html
+ *
+ *  5. Write a program to print largest number among three numbers.
+ */
+package com.jala.loops;
+
+import java.util.Scanner;
+
+public class LargestAmongNumbers {
+    public static void main(String[] args) {
+
+        int a, b, c;
+        Scanner num = new Scanner(System.in);
+        //inputs are stored in a,b,c.
+        System.out.print("Enter First Number : ");
+        a = num.nextInt();
+        System.out.print("Enter Second Number : ");
+        b = num.nextInt();
+        System.out.print("Enter Third Number : ");
+        c = num.nextInt();
+
+        //Executes if a is largest
+        if ((a > b) && (a > c)) {
+            System.out.println("The Largest Number is : " + a);
+        }
+        //Executes if b is largest
+        else if ((b > a) && (b > c)) {
+            System.out.println("The Largest Number is : " + b);
+        }
+        //Executes if a,b are not largest
+        else {
+            System.out.println("The Largest Number is : " + c);
+        }
+    }
+}
